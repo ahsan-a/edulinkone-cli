1050851a196b22073633efe6b0047a43ee64ebec
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-03-29 21:04:29 +0100

    fixed stuff

--- src/state/index.js
+++ src/state/index.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
+const path = require('path');
 
-var credentials = JSON.parse(fs.readFileSync('./src/config/credentials.json'));
-var school = JSON.parse(fs.readFileSync('./src/config/school.json'));
+var credentials = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config/credentials.json')));
+var school = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config/school.json')));
 var user = {};
 var homework = {};
 
