c376b548923d8f811707f9b958fbe7c4dbd94b72
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-03-31 17:20:26 +0100

    removed unnecesary packages

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 
 // imports
-const program = require('commander');
-
 var state = require('./state');
 const prompts = require('./prompts');
-
-//commander
-program.version('0.0.1');
 
 if (Object.keys(state.credentials).length < 1) {
 	prompts.schoolPostcode();
